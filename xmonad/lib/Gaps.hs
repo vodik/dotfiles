1a9117b9e519b2b558c79ea06eccd19d8ad8bfc2
Simon Gomizelj <simongmzlj@gmail.com>
2011-12-11 23:01:06 -0500

    Better Gaps implementation. Properly pads with 1/2 the gap instead of just sticking the window in the top-left corner.

--- xmonad/lib/Gaps.hs
+++ xmonad/lib/Gaps.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 
-module Gaps ( gaps, gapsBorder, Gaps ) where
+module Gaps ( gaps
+            , gapsBorder
+            , Gaps ) where
 
-import Graphics.X11 (Rectangle(..))
+import Data.Int
+import Data.Word
+import Graphics.X11 (Rectangle(..), Position, Dimension)
 import Control.Arrow (second)
 import XMonad.Util.Font (fi)
 import XMonad.Layout.LayoutModifier
@@ -16,16 +20,37 @@ gapsBorder s g = ModifiedLayout (Gaps s g)
 data Gaps a = Gaps Int Int deriving (Show, Read)
 
 instance LayoutModifier Gaps a where
-    pureModifier (Gaps s g) r _ wrs = (map (second $ shrinkRect s g r) wrs, Nothing)
+    pureModifier gap r _ wrs = (map (second $ shrinkRect gap r) wrs, Nothing)
     modifierDescription (Gaps _ g) = "Gaps " ++ show g
 
-shrinkRect :: Int -> Int -> Rectangle -> Rectangle -> Rectangle
-shrinkRect s g (Rectangle sx sy sw sh) (Rectangle x y w h) =
+-- | Shrink the window's rectangle to add a nice gaps between windows.
+--
+shrinkRect :: Gaps a -> Rectangle -> Rectangle -> Rectangle
+shrinkRect gap (Rectangle sx sy sw sh) (Rectangle x y w h) =
     Rectangle x' y' w' h'
     where
-        x' = if x == sx then x + (fi s) else x
-        y' = if y == sy then y + (fi s) else y
-        w' = if x == sx then w - (fi s) - gX else w - gX
-        h' = if y == sy then h - (fi s) - gY else h - gY
-        gX = if x + (fi w) - sx == fi sw then fi s else fi g
-        gY = if y + (fi h) - sy == fi sh then fi s else fi g
+        x' = x + xyCalcGap gap x sx
+        y' = y + xyCalcGap gap y sy
+        w' = xyCalcWidth gap x w sx sw
+        h' = xyCalcWidth gap y h sy sh
+
+-- | Calculate the offset from either the left or from the top to add
+-- a gap.
+--
+xyCalcGap (Gaps s g) x sx
+    | x == sx   = fi s
+    | otherwise = halfGap g
+
+-- | Calculate the new width of the window to account to make the
+-- gaps.
+--
+xyCalcWidth (Gaps s g) x w sx sw = w - xyCalcLeftGap - xyCalcRightGap
+    where xyCalcLeftGap  | onLeft    = fi s
+                         | otherwise = halfGap g
+          xyCalcRightGap | onRight   = fi s
+                         | otherwise = halfGap g
+          onLeft  = x == sx
+          onRight = x + fi w - sx == fi sw
+
+halfGap :: Integral a => Int -> a
+halfGap = truncate . (/2) . fi
