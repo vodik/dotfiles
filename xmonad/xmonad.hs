c592561c9a1bb0f851402535ec367e575a912c37
Simon Gomizelj <simongmzlj@gmail.com>
2011-12-14 20:07:01 -0500

    Almost completely rewitten. This file is almost my own now.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,131 +1,91 @@
--- XMonad modules
-
-import qualified Data.Map as M
-import Data.Ratio ((%))
 import Text.Regex.Posix ((=~))
 import System.Exit
 
 import XMonad
+
 import XMonad.Actions.CycleWS
-import qualified XMonad.Actions.Submap as SM
-import qualified XMonad.Actions.Search as S
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Grid
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.DynamicLog
 import XMonad.Layout.ResizableTile
-import XMonad.Layout.Reflect
-import XMonad.Layout.IM
 import XMonad.Layout.Tabbed
 import XMonad.Layout.PerWorkspace
-import XMonad.Layout.Spacing
+import XMonad.Layout.NoBorders
 import XMonad.Prompt.Shell
 import XMonad.Prompt
-import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 import XMonad.Util.Scratchpad
 import XMonad.Util.Run
 
+import qualified XMonad.StackSet as W
+import qualified XMonad.Actions.Search as S
+
 import Gaps
 
-myWorkspaces = [ "web", "term", "code", "chat", "doc", "virt", "games" ]
-myBrowser = "firefox"
-myTerm = "urxvtc"
-
-main = do
-    xmproc <- spawnPipe "xmobar"
-    xmonad  $ withUrgencyHook NoUrgencyHook $ defaultConfig
-        { manageHook = myManageHook
-        , handleEventHook = fullscreenEventHook
-        , layoutHook = myLayoutRules
-        , logHook = dynamicLogWithPP $ myPP xmproc
-        , modMask = mod4Mask
-        , keys = myKeys
-        , terminal = myTerm
-        , borderWidth = 2
-        , normalBorderColor = "#333333"
-        , focusedBorderColor = "#bf1e2d"
-        , workspaces = myWorkspaces
-        , focusFollowsMouse = True
-        }
-
-myLayoutRules =
-    onWorkspace "web" tabbedLayout $
-    onWorkspace "doc" tabbedLayout $
-    onWorkspace "virt" (avoidStruts $ full) $
-    standardLayouts
-
-standardLayouts = avoidStruts $
-    tiled ||| reflectTiled ||| Mirror tiled ||| grid ||| full
-
--- Layouts
-tiled        = gaps 5 $ smartBorders $ ResizableTall 1 (2/100) (1/2) []
-grid         = gaps 5 $ Grid
-reflectTiled = reflectHoriz tiled
-full         = noBorders Full
-tabLayout    = tabbedBottom shrinkText myTheme
-
--- web and doc
-tabbedLayout = avoidStruts $ smartBorders $
-    tabLayout ||| tiled ||| full
-
-myManageHook :: ManageHook
-myManageHook = composeAll
-    [ manageHook defaultConfig
-    , manageDocks
-    , myRules
-    ]
-
 q ~? x = fmap (=~ x) q
+
+myTerminal      = "urxvtc"
+myBorderWidth   = 2
+myModMask       = mod4Mask
+myWorkspaces    = [ "work", "term", "code", "chat", "virt", "games" ] ++ map show [7..9]
+
+myNormalBorderColor  = "#333333"
+myFocusedBorderColor = "#bf1e2d"
+
+myLayoutRules = avoidStruts $
+    onWorkspace "work"  (tabbed ||| tiled) $
+    onWorkspace "virt"  full $
+    onWorkspace "games" full $
+    tiled ||| Mirror tiled ||| full
+    where
+        tiled  = gaps 5 $ ResizableTall 1 (2/100) (1/2) []
+        full   = noBorders Full
+        tabbed = noBorders $ tabbedBottom shrinkText myTheme
+
 myRules = scratchpadManageHook (W.RationalRect 0.1 0.1 0.8 0.8)
     <+> (composeAll . concat $
         [ [ className =? c --> doCenterFloat   | c <- floats ]
-        , [ className =? c --> doShift "web"   | c <- web ]
-        , [ className =? c --> doShift "doc"   | c <- doc ]
+        , [ className =? c --> doShift "work"  | c <- work ]
         , [ className =? c --> doShift "virt"  | c <- virt ]
         , [ className =? c --> doShift "games" | c <- games ]
-        , [ className ~? "^[Ll]ibre[Oo]ffice" --> doShift "doc"
+        , [ className ~? "^[Ll]ibre[Oo]ffice" --> doShift "work"
           , resource  =? "desktop_window"     --> doIgnore
           , isFullscreen                      --> doFullFloat
           , isDialog                          --> doCenterFloat
           ]
         ])
-    <+> manageDocks
+    where
+        floats = [ "Xmessage", "Mplayer", "Lxappearance", "Nitrogen" ]
+        work   = [ "Firefox", "Chromium", "Zathura" ]
+        virt   = [ "VirtualBox" ]
+        games  = [ "Sol", "net-minecraft-LauncherFrame"]
 
-floats = [ "Xmessage", "Mplayer", "Lxappearance", "Nitrogen" ]
-web    = [ "Firefox", "Chromium" ]
-doc    = [ "Zathura" ]
-virt   = [ "VirtualBox" ]
-games  = [ "Sol", "net-minecraft-LauncherFrame"]
-
-myKeys conf = mkKeymap conf $
+myKeys browser conf = mkKeymap conf $
     -- terminal
     [ ("M-<Return>", spawn $ XMonad.terminal conf)
-    , ("M-w", spawn $ myBrowser)
-    , ("M-S-w", spawn $ unwords [ myBrowser, "http://www.gmail.com" ])
-    -- , ("M-S-w", spawn $ unwords [ myBrowser, "http://www.reddit.com" ])
-    -- , ("M-S-w", spawn $ unwords [ myBrowser, "http://www.arstechnica.com" ])
     , ("M-e", scratchpadSpawnActionTerminal $ XMonad.terminal conf)
+    , ("M-p", shellPrompt myXPConfig)
+
+    -- browser
+    , ("M-w",     spawn $ browser)
+    , ("M-S-w w", spawn $ unwords [ browser, "http://www.gmail.com" ])
+    , ("M-S-w r", spawn $ unwords [ browser, "http://www.reddit.com" ])
+    , ("M-S-w a", spawn $ unwords [ browser, "http://www.arstechnica.com" ])
 
     -- quit, or restart
-    , ("M-C-q", io (exitWith ExitSuccess))
+    , ("M-C-q", io $ exitWith ExitSuccess)
     , ("M-S-c", kill)
     , ("M-S-q", restart "xmonad" True)
 
-    -- screenshot
-    , ("C-<Print>", spawn "sleep 0.2; scrot -s -e 'mv $f ~/images/screenshots/'")
-    , ("<Print>", spawn "scrot -e 'mv $f ~/images/screenshots/'")
-
-    -- launcher
-    , ("M-p", shellPrompt myXPConfig)
-
     -- layout
-    , ("M-n", sendMessage NextLayout)
+    , ("M-n",   sendMessage NextLayout)
     , ("M-S-n", sendMessage FirstLayout)
+
+    -- resizing
+    , ("M-h", sendMessage Shrink)
+    , ("M-l", sendMessage Expand)
 
     -- focus
     , ("M-<Tab>", windows W.focusDown)
@@ -150,24 +110,19 @@ myKeys conf = mkKeymap conf $
     , ("M-S-j", windows W.swapDown)
     , ("M-S-k", windows W.swapUp)
 
-    -- resizing
-    , ("M-h", sendMessage Shrink)
-    , ("M-l", sendMessage Expand)
-
     -- multimedia keys
     , ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 5%-")
     , ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master on 5%+")
-    , ("<XF86AudioMute>", spawn "amixer -q set Master toggle")
+    , ("<XF86AudioMute>",        spawn "amixer -q set Master toggle")
 
     -- mpd controls
     , ("<XF86AudioPlay>", spawn "mpc toggle")
     , ("<XF86AudioNext>", spawn "mpc next")
     , ("<XF86AudioPrev>", spawn "mpc prev")
 
-    -- mplayer controls
-    , ("C-<XF86AudioPlay>", mPlay "pause")
-    , ("C-<XF86AudioNext>", mPlay "pt_step 1")
-    , ("C-<XF86AudioPrev>", mPlay "pt_step -1")
+    -- screenshot
+    , ("C-<Print>", spawn "sleep 0.1; scrot -s -e 'mv $f ~/pictures/screenshots/'")
+    , ("<Print>",   spawn "scrot -e 'mv $f ~/pictures/screenshots/'")
 
     -- backlight hack
     , ("M-x", spawn "xbacklight -set 100%")
@@ -176,54 +131,69 @@ myKeys conf = mkKeymap conf $
     [ ("M-s " ++ k, S.promptSearch myXPConfig f) | (k, f) <- searchList ]
     ++
     [ (m ++ [i], f w) | (i, w) <- zip ['1'..] $ workspaces conf
-                      , (m, f) <- [ ("M-", windows . W.greedyView)
-                                  , ("M-S-", windows . W.shift)
-                                  ]
+                       , (m, f) <- [ ("M-", windows . W.greedyView)
+                                   , ("M-S-", windows . W.shift)
+                                   ]
     ]
 
-mPlay cmd = spawn $ unwords ["echo", cmd, "> ~/.mplayer_fifo"]
-
 searchList :: [([Char], S.SearchEngine)]
-searchList = [ ("g",   S.google)
-             , ("w",   S.wikipedia)
-             , ("r",   S.searchEngine "reddit" "http://www.reddit.com/search?q=")
-             , ("S-w", S.alpha)
-             , ("y",   S.youtube)
-             , ("S-a", S.amazon)
-             , ("a",   S.searchEngine "archwiki" "http://wiki.archlinux.org/index.php/Special:Search?search=")
-             , ("h",   S.hoogle)
-             , ("i",   S.isohunt)
-             , ("d",   S.searchEngine "wiktionary" "http://en.wiktionary.org/w/index.php/Special:Search?search=")
-             ]
+searchList =
+    [ ("g",   S.google)
+    , ("w",   S.wikipedia)
+    , ("y",   S.youtube)
+    , ("h",   S.hoogle)
+    , ("S-w", S.alpha)
+    , ("S-a", S.amazon)
+    , ("a",   S.searchEngine "archwiki" "http://wiki.archlinux.org/index.php/Special:Search?search=")
+    , ("r",   S.searchEngine "reddit" "http://www.reddit.com/search?q=")
+    , ("d",   S.searchEngine "wiktionary" "http://en.wiktionary.org/w/index.php/Special:Search?search=")
+    , ("t",   S.searchEngine "piratebay" "http://thepiratebay.org/search/")
+    ]
 
--- themeing for tab layout
-myTheme = defaultTheme
-    { decoHeight = 18
-    , activeColor = "#bf1e2d"
-    , activeBorderColor = "#ff0000"
-    , activeTextColor = "#000000"
-    , inactiveBorderColor = "#000000"
-    }
+main = do
+    browser <- getBrowser
+    xmproc  <- spawnPipe "xmobar"
+    xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
+        { manageHook = manageHook defaultConfig <+> manageDocks <+> myRules
+        , handleEventHook = fullscreenEventHook
+        , layoutHook = myLayoutRules
+        , logHook = dynamicLogWithPP $ myPP xmproc
+        , modMask = mod4Mask
+        , keys = myKeys browser
+        , terminal = myTerminal
+        , borderWidth = 2
+        , normalBorderColor = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
+        , workspaces = myWorkspaces
+        , focusFollowsMouse = True
+        }
 
 myPP output = defaultPP
-    { ppCurrent = xmobarColor "#7B79B1" "#0F141F" . wrap "[" "]"
+    { ppCurrent = xmobarColor "#7b79b1" "#0f141f" . wrap "[" "]"
     , ppVisible = wrap "(" ")"
     , ppHiddenNoWindows = const ""
-    , ppSep    = " -> "
-    , ppTitle  = xmobarColor "#7B79B1" "" . shorten 50
-    , ppUrgent = xmobarColor "#bf1e2d" "0FA3A3" . wrap "<" ">"
+    , ppSep    = " | "
+    , ppTitle  = xmobarColor "#7b79b1" "" . shorten 150
+    , ppUrgent = xmobarColor "#f92672" "#0f141f"
     , ppWsSep  = " "
     , ppLayout = const ""
     , ppOrder  = \(ws:_:t:_) -> [ws,t]
     , ppOutput = hPutStrLn output
     }
 
--- some nice colors for the prompt
+myTheme = defaultTheme
+    { decoHeight = 18
+    , activeColor       = "#bf1e2d"
+    , activeBorderColor = "#ff0000"
+    , activeTextColor   = "#000000"
+    }
+
 myXPConfig = defaultXPConfig
     { font     = "xft:Envy Code R:size=9"
-    , fgColor  = "#00FFFF"
-    , bgColor  = "#151515"
-    , bgHLight = "#151515"
-    , fgHLight = "#FF0000"
+    , fgColor  = "#8cedff"
+    , bgColor  = "black"
+    , bgHLight = "black"
+    , fgHLight = "#f92672"
     , position = Bottom
+    , promptBorderWidth = 0
     }
