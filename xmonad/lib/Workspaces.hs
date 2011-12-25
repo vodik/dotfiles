a705079434f4e19805c655dbc41f75ce8a46a3c2
Simon Gomizelj <simongmzlj@gmail.com>
2011-12-25 05:42:09 -0500

    Use list compehension to build the icon map.

--- xmonad/lib/Workspaces.hs
+++ xmonad/lib/Workspaces.hs
@@ -23,11 +23,8 @@ getWSIcon (Workspace _ i _) = i
 filterWS :: String -> [Workspace] -> [Workspace]
 filterWS name = filter $ (name /=) . getWSName
 
--- TODO: there's a more efficient way of doing this
 getIconMap :: [Workspace] -> M.Map String String
-getIconMap ws = M.fromList $ zip names icons
-    where names = map getWSName ws
-          icons = map getWSIcon ws
+getIconMap ws = M.fromList [ (n, i) | (Workspace n i _) <- ws ]
 
 workspaceRules :: (String -> Property) -> [Workspace] -> Query (Endo WindowSet)
 workspaceRules c (Workspace n _ prop:xs) =
