e3cd7204d209dc084b58cde40b1471f2109174c2
Simon Gomizelj <simongmzlj@gmail.com>
2012-04-22 06:53:52 -0400

    Cleaned up ssh zstyle, removed the ssh zstyle since as of zsh 4.3.9 it does all this automatically.

--- zsh/zstyle.zsh
+++ zsh/zstyle.zsh
@@ -12,50 +12,44 @@ _force_rehash() {
   return 1
 }
 
-zstyle ':completion::complete:*'          use-cache 1
+zstyle ':completion::complete:*'               use-cache 1
 
-# format autocompletion style
-zstyle ':completion:*:descriptions'       format "%{$c1%}%d%{$reset_color%}"
-zstyle ':completion:*:corrections'        format "%{$c3%}%d%{$reset_color%}"
-zstyle ':completion:*:messages'           format "%{$c1%}%d%{$reset_color%}"
-zstyle ':completion:*:warnings'           format "%{$c1%}%d%{$reset_color%}"
-zstyle ':completion:*:default'            list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:descriptions'            format "%{$c1%}%d%{$reset_color%}"
+zstyle ':completion:*:corrections'             format "%{$c3%}%d%{$reset_color%}"
+zstyle ':completion:*:messages'                format "%{$c1%}%d%{$reset_color%}"
+zstyle ':completion:*:warnings'                format "%{$c1%}%d%{$reset_color%}"
+zstyle ':completion:*:default'                 list-colors ${(s.:.)LS_COLORS}
 
-zstyle ':completion:*'                    matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*'                    list-colors ''
-zstyle ':completion:*'                    completer _oldlist _expand _force_rehash _complete _match _approximate
-zstyle ':completion:*'                    menu select=2
+zstyle ':completion:*'                         matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*'                         list-colors ''
+zstyle ':completion:*'                         completer _oldlist _expand _force_rehash _complete _match _approximate
+zstyle ':completion:*'                         menu select=2
 
-zstyle ':completion:*:functions'          ignored-patterns '_*'
-zstyle ':completion:*:match:*'            original only
-zstyle ':completion:*:approximate:*'      max-errors 1 numeric
+zstyle ':completion:*:functions'               ignored-patterns '_*'
+zstyle ':completion:*:match:*'                 original only
+zstyle ':completion:*:approximate:*'           max-errors 1 numeric
 
-zstyle ':completion:*:cd:*'               tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:*:*:users'               ignored-patterns \
+                                                   bin daemon mail ftp http nobody dbus avahi named git bitlbee mpd \
+                                                   rtkit ntp usbmux gdm
 
-# PACMAN {{{2
-zstyle ':completion:*:pacman:*'           force-list always
-zstyle ':completion:*:*:pacman:*'         menu yes select
+# COMMANDS {{{1
 
-# KILL {{{2
-zstyle ':completion:*:kill:*'             force-list always
-zstyle ':completion:*:*:kill:*'           command 'ps -e -o pid,%cpu,tty,cputime,cmd'
-zstyle ':completion:*:*:kill:*'           menu yes select
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:cd:*'                    tag-order local-directories directory-stack path-directories
 
-# VIM {{{2
-zstyle ':completion:*:*:(vim):*:*files'   ignored-patterns '*~|*.(old|bak|o|hi)'
-zstyle ':completion:*:*:(vim):*:*files'   file-sort modification
-zstyle ':completion:*:*:(vim):*'          file-sort modification
-zstyle ':completion:*:*:(vim):*'          tag-order files
+# pacman zstyle {{{2
+zstyle ':completion:*:pacman:*'                force-list always
+zstyle ':completion:*:*:pacman:*'              menu yes select
 
-# SSH {{{2
-_etc_hosts=()
-[[ -r /etc/hosts ]] && : ${(A)_etc_hosts:=${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}} || _etc_hosts=()
+# vim {{{2
+zstyle ':completion:*:*:(vim|gvim):*:*files'   ignored-patterns '*~|*.(old|bak|o|hi)'
+zstyle ':completion:*:*:(vim|gvim):*:*files'   file-sort modification
+zstyle ':completion:*:*:(vim|gvim):*'          file-sort modification
+zstyle ':completion:*:*:(vim|gvim):*'          tag-order files
 
-zstyle ':completion:*:hosts'              hosts $_etc_hosts
-unset _etc_hosts
+# kill zstyle {{{2
+zstyle ':completion:*:kill:*'                  force-list always
+zstyle ':completion:*:*:kill:*'                command 'ps -e -o pid,%cpu,tty,cputime,cmd'
+zstyle ':completion:*:*:kill:*'                menu yes select
+zstyle ':completion:*:*:kill:*:processes'      list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
-# Don't complete uninteresting users
-zstyle ':completion:*:*:*:users' ignored-patterns \
-  bin daemon mail ftp http nobody dbus avahi named git bitlbee mpd \
-  rtkit ntp usbmux gdm
