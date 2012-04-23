a13e50bba8ab971c78b3a901b3974d6684d4ed8e
Simon Gomizelj <simongmzlj@gmail.com>
2012-04-23 06:32:23 -0400

    Updated zstyles, removed hack from t script.

--- zsh/zstyle.zsh
+++ zsh/zstyle.zsh
@@ -12,13 +12,21 @@ _force_rehash() {
   return 1
 }
 
-zstyle ':completion::complete:*'               use-cache 1
+zstyle ':completion::complete:*'               use-cache on
 
-zstyle ':completion:*:descriptions'            format "%{$c1%}%d%{$reset_color%}"
-zstyle ':completion:*:corrections'             format "%{$c3%}%d%{$reset_color%}"
-zstyle ':completion:*:messages'                format "%{$c1%}%d%{$reset_color%}"
-zstyle ':completion:*:warnings'                format "%{$c1%}%d%{$reset_color%}"
-zstyle ':completion:*:default'                 list-colors ${(s.:.)LS_COLORS}
+# Group matches and describe.
+zstyle ':completion:*:*:*:*:*'                 menu yes select
+zstyle ':completion:*:matches'                 group 'yes'
+zstyle ':completion:*:options'                 description 'yes'
+zstyle ':completion:*:options'                 auto-description '%d'
+zstyle ':completion:*:corrections'             format '%B%F{green}>> %d (errors: %e)%f%b'
+zstyle ':completion:*:descriptions'            format '%B%F{magenta}>> %d%f%b'
+zstyle ':completion:*:messages'                format '%B%F{cyan}>> %d%f%b'
+zstyle ':completion:*:warnings'                format '%B%F{red}>> no matches found%f%b'
+zstyle ':completion:*:default'                 list-prompt '%B%S%M matches%s%b'
+zstyle ':completion:*'                         format '%B%F{cyan}>> %d%f%b'
+zstyle ':completion:*'                         group-name ''
+zstyle ':completion:*'                         verbose yes
 
 zstyle ':completion:*'                         matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*'                         list-colors ''
@@ -34,21 +42,31 @@ zstyle ':completion:*:*:*:users'               ignored-patterns \
                                                    rtkit ntp usbmux gdm
 
 # COMMANDS {{{1
-zstyle ':completion:*:cd:*'                    tag-order local-directories directory-stack path-directories
+zstyle ':completion:*'                         list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*:*:cd:*'                  tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:-tilde-:*'               group-order 'named-directories' 'path-directories' 'users' 'expand'
+zstyle ':completion:*'                         squeeze-slashes true
 
-# pacman zstyle {{{2
-zstyle ':completion:*:pacman:*'                force-list always
-zstyle ':completion:*:*:pacman:*'              menu yes select
-
-# vim {{{2
+# vim zstyle {{{2
 zstyle ':completion:*:*:(vim|gvim):*:*files'   ignored-patterns '*~|*.(old|bak|o|hi)'
 zstyle ':completion:*:*:(vim|gvim):*:*files'   file-sort modification
 zstyle ':completion:*:*:(vim|gvim):*'          file-sort modification
 zstyle ':completion:*:*:(vim|gvim):*'          tag-order files
 
 # kill zstyle {{{2
-zstyle ':completion:*:kill:*'                  force-list always
 zstyle ':completion:*:*:kill:*'                command 'ps -e -o pid,%cpu,tty,cputime,cmd'
-zstyle ':completion:*:*:kill:*'                menu yes select
 zstyle ':completion:*:*:kill:*:processes'      list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:*:kill:*'                insert-ids single
 
+# man {{{2
+zstyle ':completion:*:manuals'                 separate-sections true
+zstyle ':completion:*:manuals.(^1*)'           insert-sections true
+
+# ssh/scp/rsync {{{2
+zstyle ':completion:*:(scp|rsync):*'                  tag-order 'hosts:-host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
+zstyle ':completion:*:(scp|rsync):*'                  group-order files all-files hosts-domain hosts-host hosts-ipaddr
+zstyle ':completion:*:ssh:*'                          tag-order 'hosts:-host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
+zstyle ':completion:*:ssh:*'                          group-order hosts-domain hosts-host users hosts-ipaddr
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host'   ignored-patterns '*.*' loopback localhost
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain' ignored-patterns '<->.<->.<->.<->' '^*.*' '*@*'
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr' ignored-patterns '^<->.<->.<->.<->' '127.0.0.<->'
