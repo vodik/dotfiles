2b270207f6412c9bd202119185abe0f66da4d897
Simon Gomizelj <simongmzlj@gmail.com>
2012-05-03 04:15:22 -0400

    Updated aliases.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,11 +1,10 @@
-alias ls="ls -FNh --tabsize=0 --color=auto --show-control-chars --group-directories-first"
-alias ll="ls -l"
-alias la="ls -a"
-alias lp="ls -a --color=always | $PAGER -R"
+alias ls='ls -FNh --tabsize=0 --color=auto --show-control-chars --group-directories-first'
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias tree='tree -A'
 
-alias vir="vim --servername vim --remote-tab-silent"
-
-alias history="fc -l 1"
+alias please='sudo $(history -n -1)'
 
 # stderred support (https://github.com/albinoloverats/stderred)
 alias _='LD_PRELOAD=/usr/lib/stderred.so'
