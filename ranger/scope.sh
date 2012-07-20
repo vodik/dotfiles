66cea25cb4e7c81f6b5339d7016fd33f68b64007
Simon Gomizelj <simongmzlj@gmail.com>
2012-07-20 16:16:01 -0400

    Cleanup scope.sh

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -22,58 +22,59 @@ path="$1"    # Full path of the selected file
 width="$2"   # Width of the preview pane (number of fitting characters)
 height="$3"  # Height of the preview pane (number of fitting characters)
 
-maxln=200    # Stop after $maxln lines.  Can be used like ls | head -n $maxln
+have() { type -P "$1" > /dev/null; }
+trim() { head -n ${1:-$height}; }
 
-# Find out something about the file:
-mimetype=$(file --mime-type -Lb "$path")
-extension=${path##*.}
+success() {
+  case ${PIPESTATUS[0]} in
+    0|141) exit 0;;
+    *)     exit 1;;
+  esac
+}
 
-# Functions:
-# "have $1" succeeds if $1 is an existing command/installed program
-function have { type -P "$1" > /dev/null; }
-# "success" returns the exit code of the first program in the last pipe chain
-function success { test ${PIPESTATUS[0]} = 0; }
-
+extension="${path##*.}"
 case "$extension" in
-	# Archive extensions:
-	7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
-	rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
-		als "$path" | head -n $maxln
-		success && exit 0 || acat "$path" | head -n $maxln && exit 3
-		exit 1;;
-	# PDF documents:
-	pdf)
-		pdftotext -l 10 -nopgbrk -q "$path" - | head -n $maxln | fmt -s -w $width
-		success && exit 0 || exit 1;;
-	# BitTorrent Files
-	torrent)
-		transmission-show "$path" | head -n $maxln && exit 3
-		success && exit 5 || exit 1;;
-	# HTML Pages:
-	htm|html|xhtml)
-		have w3m    && w3m    -dump "$path" | head -n $maxln | fmt -s -w $width && exit 4
-		have lynx   && lynx   -dump "$path" | head -n $maxln | fmt -s -w $width && exit 4
-		have elinks && elinks -dump "$path" | head -n $maxln | fmt -s -w $width && exit 4
-		;; # fall back to highlight/cat if theres no lynx/elinks
+  7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
+  rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
+    als "$path" | trim
+    success && exit 0 || acat "$path" | trim && exit 3
+    exit 1;;
+
+  pdf)
+    pdftotext -l 10 -nopgbrk -q "$path" - | trim | fmt -s -w $width
+    success && exit 0 || exit 1;;
+
+  doc)
+    catdoc "$path" | trim | fmt -s -w $width
+    success && exit 0 || exit 1;;
+
+  torrent)
+    transmission-show "$path" | trim && exit 3
+    success && exit 5 || exit 1;;
+
+  htm|html|xhtml)
+    have w3m    && w3m    -dump "$path" | trim | fmt -s -w $width && exit 4
+    have lynx   && lynx   -dump "$path" | trim | fmt -s -w $width && exit 4
+    have elinks && elinks -dump "$path" | trim | fmt -s -w $width && exit 4
+    ;; # fall back to highlight/cat if theres no lynx/elinks
 esac
 
+mimetype=$(file --mime-type -Lb "$path")
 case "$mimetype" in
-	# Syntax highlight for text files:
-	text/* | */xml)
-		highlight --out-format=ansi "$path" | head -n $maxln
-		success && exit 5 || exit 2;;
-	# Ascii-previews of images:
-	image/*)
-		img2txt --gamma=0.6 --width="$width" "$path" && exit 4 || exit 1;;
-	# Display information about media files:
-	video/* | audio/*)
-		have exiftool && exiftool "$path" && exit 5
-		# Use sed to remove spaces so the output fits into the narrow window
-		if have mediainfo; then
-			mediainfo "$path" | sed 's/  \+:/: /;'
-			success && exit 5
-		fi
-		exit 1;;
+  text/*|*/xml)
+    highlight --out-format=ansi "$path" | trim
+    success && exit 5 || exit 2;;
+
+  image/*)
+    img2txt --gamma=0.6 --width="$width" "$path" && exit 4 || exit 1;;
+
+  video/*|audio/*)
+    have exiftool && exiftool "$path" && exit 5
+    if have mediainfo; then
+      mediainfo "$path" | sed 's/  \+:/: /;'
+      success && exit 5
+    fi
+    exit 1;;
 esac
 
 exit 1
