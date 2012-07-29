b9fa85e8abadc2090b0b09d6c007e43e2b7e459b
Simon Gomizelj <simongmzlj@gmail.com>
2012-07-29 14:10:40 -0400

    Add spawnWithEnv

--- xmonad/lib/XMonad/Util/Commands.hs
+++ xmonad/lib/XMonad/Util/Commands.hs
@@ -8,6 +8,7 @@ module XMonad.Util.Commands
     , runWith
     , delayedSpawn
     , spawnIn
+    , spawnWithEnv
     ) where
 
 import Codec.Binary.UTF8.String (encodeString)
@@ -15,6 +16,7 @@ import Control.Applicative
 import Control.Concurrent (threadDelay)
 import Control.Monad
 import System.Directory (setCurrentDirectory)
+import System.Posix.Env
 import System.Posix.Process (createSession, executeFile, forkProcess)
 import System.Posix.Types (ProcessID(..))
 import XMonad hiding (spawn)
@@ -46,15 +48,16 @@ spawn :: (MonadIO m, Command c) => c -> m ()
 spawn = void_ . run
 
 runWith :: (MonadIO m, Command c) => IO () -> c -> m ProcessID
-runWith f c = xfork $ f >> exec c
+runWith f c = xfork $ catchIO f >> exec c
 
 delayedSpawn :: (MonadIO m, Command c) => Int -> c -> m ()
 delayedSpawn d cmd = io (threadDelay d) >> spawn cmd
 
 spawnIn :: (MonadIO m, Command c) => FilePath -> c -> m ()
-spawnIn dir = void_ . runWith (setDir dir)
-  where
-    setDir = catchIO . setCurrentDirectory
+spawnIn dir = void_ . runWith (setCurrentDirectory dir)
+
+spawnWithEnv :: (MonadIO m, Command c) => [String] -> c -> m ()
+spawnWithEnv env = void_ . runWith (mapM_ putEnv env)
 
 void_ :: Monad m => m a -> m ()
 void_ = (>>= const (return ()))
