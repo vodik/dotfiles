8a17178468cf6407c34c5c25dfd855ddb96d676f
Simon Gomizelj <simongmzlj@gmail.com>
2012-09-17 18:38:43 -0400

    add status information to zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,17 +1,26 @@
 setopt prompt_subst
-autoload -Uz vcs_info
+autoload -U vcs_info
 
 zstyle ':vcs_info:*'              enable        git cvs svn
 zstyle ':vcs_info:*'              actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
 zstyle ':vcs_info:*'              formats       '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat  '%b%F{1}:%F{3}%r'
 
-vcs_info_wrapper() {
+vimode=I
+# set vimode to current editing mode
+function zle-line-init zle-keymap-select {
+  vimode="${${KEYMAP/vicmd/C}/(main|viins)/I}"
+  zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+function vcs_info_wrapper {
   vcs_info
-  [[ -n "$vcs_info_msg_0_" ]] && echo "%{$fg[grey]%}${vcs_info_msg_0_/ /}%{$reset_color%}"
+  [[ -n "$vcs_info_msg_0_" ]] && echo " %{$fg[grey]%}${vcs_info_msg_0_/ /}%{$reset_color%}"
 }
 
-set_prompt() {
+function set_prompt {
   local pmt="red"
 
   (( EUID == 0 ))          && pmt="blue"
@@ -21,9 +30,10 @@ set_prompt() {
   (( EUID == 0 )) && PROMPT+="%{$fg[red]%}##%{$reset_color%} " \
                   || PROMPT+="%{$fg[blue]%}//%{$reset_color%} "
 
-  RPROMPT=$'$(vcs_info_wrapper)'
+  RPROMPT='$(vcs_info_wrapper)'
   RPROMPT+="%{$fg[yellow]%}%(?.. %?)%{$reset_color%}"
+  RPROMPT+='%{$fg[green]%} ${vimode}%{$reset_color%}'
 }
 
 set_prompt
-unset set_promt
+unset set_prompt
