44e9ea04b85809271d24accabe49f15da1813980
Simon Gomizelj <simongmzlj@gmail.com>
2013-01-15 22:04:21 -0500

    add more aliases

--- shell/zsh/alias.zsh
+++ shell/zsh/alias.zsh
@@ -11,7 +11,14 @@ alias netcfg='sudo netcfg'
 alias netcfg-menu='sudo netcfg-menu'
 alias wifi-select='sudo wifi-select'
 
+alias startx='exec \startx'
+
 alias x='exec startx'
 
 # stderred support (https://github.com/albinoloverats/stderred)
 alias _='LD_PRELOAD=/usr/lib/libstderred.so'
+
+usegcc() {
+  [[ $CC  == gcc ]] && export CC=clang    || export CC=gcc
+  [[ $CXX == g++ ]] && export CXX=clang++ || export CXX=g++
+}
