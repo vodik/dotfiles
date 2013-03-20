3d3560af20434161f228585164ed2910476d828c
Simon Gomizelj <simongmzlj@gmail.com>
2013-03-20 02:21:00 -0400

    vim: ycm configs for c and c++

---
+++ vim/ftplugin/cpp.vim
@@ -0,0 +1,1 @@
+let g:ycm_global_ycm_extra_conf = '~/.vim/ycm/cpp_conf.py'
--- vim/ycm/cpp_conf.py
+++ vim/ycm/cpp_conf.py
@@ -12,50 +12,25 @@ compilation_database_folder = ''
 # These are the compilation flags that will be used in case there's no
 # compilation database set.
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wc++98-compat',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-'-DUSE_CLANG_COMPLETER',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-'-std=c++11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-isystem',
-'../BoostParts',
-'-isystem',
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-'/System/Library/Frameworks/Python.framework/Headers',
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I',
-'.',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include'
+  '-Wall',
+  '-Wextra',
+  '-Werror',
+  '-Wno-long-long',
+  '-Wno-variadic-macros',
+  '-fexceptions',
+  '-DNDEBUG',
+  '-DUSE_CLANG_COMPLETER',
+  '-std=c++11'
+  '-x', 'c++',
+  '-isystem', '../BoostParts',
+  '-isystem', '../llvm/include',
+  '-isystem', '../llvm/tools/clang/include',
+  '-I',       '.',
+  '-I',       './ClangCompleter',
+  '-isystem', './tests/gmock/gtest',
+  '-isystem', './tests/gmock/gtest/include',
+  '-isystem', './tests/gmock',
+  '-isystem', './tests/gmock/include'
 ]
 
 if compilation_database_folder:
